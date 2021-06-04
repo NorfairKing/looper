c2fc013d2da2ab2528ac49739e8faefa47212e66
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-04 15:33:40 +0200

    description for the looper options in nix

--- nix/looper-option.nix
+++ nix/looper-option.nix
@@ -2,6 +2,7 @@
 with lib;
 name: mkOption {
   default = null;
+  description = "The ${name} looper";
   type = types.nullOr (types.submodule {
     options = {
       enable = mkEnableOption "${name} looper";
@@ -9,11 +10,13 @@ name: mkOption {
         type = types.nullOr types.int;
         default = null;
         example = 60;
+        description = "How long to wait before the first activation of the ${name} looper.";
       };
       period = mkOption {
         type = types.nullOr types.int;
         default = null;
         example = 0;
+        description = "How long to wait after each activation of the ${name} looper.";
       };
     };
   });
