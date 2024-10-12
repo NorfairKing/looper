ed0b5953bbe42b18918fe9367670fa8632cc9bf6
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-10-13 01:03:39 +0200

    Update horizon

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     nixpkgs-23_11.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
-    nixpkgs-23_05.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
     horizon-advance.url = "git+https://gitlab.horizon-haskell.net/package-sets/horizon-advance";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     autodocodec.url = "github:NorfairKing/autodocodec";
@@ -24,7 +23,6 @@
     { self
     , nixpkgs
     , nixpkgs-23_11
-    , nixpkgs-23_05
     , horizon-advance
     , pre-commit-hooks
     , autodocodec
@@ -62,8 +60,7 @@
           backwardCompatibilityCheckFor = nixpkgs: (haskellPackagesFor nixpkgs).autodocodecRelease;
           allNixpkgs = {
             inherit
-              nixpkgs-23_11
-              nixpkgs-23_05;
+              nixpkgs-23_11;
           };
           backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
         in
